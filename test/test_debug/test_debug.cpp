955c6f08a9f4f3719b9af67caa4d411d23a1bbda
n30nex <blushellneon@outlook.com>
2026-06-04 12:05:22 -0400

    cover debug level bounds

---
+++ test/test_debug/test_debug.cpp
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2025 Ben
+//
+// This file is part of SigurdOS.
+//
+// SigurdOS is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SigurdOS is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with SigurdOS.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <gtest/gtest.h>
+
+#include "diagnostics/debug.h"
+
+namespace {
+
+TEST(DebugTest, LevelBoundsAreDocumented) {
+    EXPECT_EQ(sigurdos::debug::SIGURDOS_DEBUG_MIN_LEVEL, 1);
+    EXPECT_EQ(sigurdos::debug::SIGURDOS_DEBUG_MAX_LEVEL, 3);
+}
+
+TEST(DebugTest, ClampLevelKeepsValidLevels) {
+    EXPECT_EQ(sigurdos::debug::clamp_level(1), 1);
+    EXPECT_EQ(sigurdos::debug::clamp_level(2), 2);
+    EXPECT_EQ(sigurdos::debug::clamp_level(3), 3);
+}
+
+TEST(DebugTest, ClampLevelRejectsBelowMinimum) {
+    EXPECT_EQ(sigurdos::debug::clamp_level(-1), 1);
+    EXPECT_EQ(sigurdos::debug::clamp_level(0), 1);
+}
+
+TEST(DebugTest, ClampLevelRejectsAboveMaximum) {
+    EXPECT_EQ(sigurdos::debug::clamp_level(4), 3);
+    EXPECT_EQ(sigurdos::debug::clamp_level(255), 3);
+}
+
+TEST(DebugTest, NonDebugBuildStubsRemainDisabled) {
+    EXPECT_EQ(sigurdos::debug::get_level(), 0);
+    EXPECT_EQ(sigurdos::debug::feat_to_mask(), 0);
+    EXPECT_FALSE(sigurdos::debug::feat_get_display());
+    EXPECT_FALSE(sigurdos::debug::feat_get_mesh());
+    EXPECT_FALSE(sigurdos::debug::feat_get_ui());
+    EXPECT_FALSE(sigurdos::debug::feat_get_map());
+    EXPECT_FALSE(sigurdos::debug::feat_get_diag());
+}
+
+} // namespace
